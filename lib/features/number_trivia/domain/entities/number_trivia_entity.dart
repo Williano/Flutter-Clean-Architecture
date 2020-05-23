aa1852e40744984d1ed9fab160fe4e1b82c52894
Williano <paawilly17@gmail.com>
2020-05-23 13:54:45 -0500

    Add entity

---
+++ lib/features/number_trivia/domain/entities/number_trivia_entity.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class NumberTrivia extends Equatable {
+  final String text;
+  final int number;
+
+  const NumberTrivia({@required this.text, @required this.number});
+
+  @override
+  List<Object> get props => [text, number];
+}
