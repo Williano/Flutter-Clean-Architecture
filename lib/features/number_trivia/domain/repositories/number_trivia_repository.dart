d35f49148796e1c67acbcfb443df86169250d563
Williano <paawilly17@gmail.com>
2020-05-23 14:16:23 -0500

    Add repository contract

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture/core/error/failures_error.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/domain/entities/number_trivia_entity.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
