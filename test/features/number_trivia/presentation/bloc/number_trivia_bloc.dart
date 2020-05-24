09fbe41990023715293051d1cc1f87ea7b5c9fd8
Williano <paawilly17@gmail.com>
2020-05-23 21:32:00 -0500

    Update

--- test/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -3,13 +3,12 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   @override
   NumberTriviaState get initialState => NumberTriviaInitial();
   @override
   Stream<NumberTriviaState> mapEventToState(
     NumberTriviaEvent event,
-  ) async* {
-    // TODO: implement mapEventToState
-  }
+  ) async* {}
 }
