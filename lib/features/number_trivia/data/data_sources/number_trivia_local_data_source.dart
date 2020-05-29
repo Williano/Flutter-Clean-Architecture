5b3dc8b2cc40402c0799224d3fa343739db33537
Williano <paawilly17@gmail.com>
2020-05-29 18:46:14 -0500

    Add test and implementation for local cache

--- lib/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter_clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:meta/meta.dart';
 
 abstract class NumberTriviaLocalDataSource {
   /// Gets the cached [NumberTriviaModel] which was gotten the last time
@@ -8,4 +12,25 @@ abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
 
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+const CACHED_NUMBER_TRIVIA = "CACHED_NUMBER_TRIVIA";
+
+class NumberTriviaLocalDataSourceImplementation
+    implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  const NumberTriviaLocalDataSourceImplementation(
+      {@required this.sharedPreferences});
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    return null;
+  }
 }
---
+++ test/features/number_trivia/data/data_sources/number_trivia_local_data_source_test.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter_clean_architecture/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../core/fixtures/fixture_reader.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  NumberTriviaLocalDataSourceImplementation localDataSourceImplmentation;
+  MockSharedPreferences mockSharedPreferences;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    localDataSourceImplmentation = NumberTriviaLocalDataSourceImplementation(
+        sharedPreferences: mockSharedPreferences);
+  });
+
+  group("getLastNumberTrivia", () {
+    final testNumberTriviaModel = NumberTriviaModel.fromJson(
+        json.decode(fixtureReader("trivia_cached.json")));
+    test(
+        "should return NumberTrivia from sharedPreferences when there is one in the cache",
+        () async {
+      //arrange
+      when(mockSharedPreferences.getString(any))
+          .thenReturn(fixtureReader("trivia_cached.json"));
+      //act
+      final result = await localDataSourceImplmentation.getLastNumberTrivia();
+
+      //assert
+      verify(mockSharedPreferences.getString(CACHED_NUMBER_TRIVIA));
+      expect(result, equals(testNumberTriviaModel));
+    });
+  });
+}
