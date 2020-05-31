fbba12961800b5cd355f5e014302757bc235423c
Williano <paawilly17@gmail.com>
2020-05-30 20:15:47 -0500

    Add implementation

--- lib/features/number_trivia/data/data_sources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/data_sources/number_trivia_remote_data_source.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter_clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:meta/meta.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   /// Calls the http://numbersapi.com/{number} endpoint.
@@ -10,4 +14,26 @@ abstract class NumberTriviaRemoteDataSource {
   ///
   /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImplementation
+    implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  const NumberTriviaRemoteDataSourceImplementation({@required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
+    final response = await client.get(
+      "http://numbersapi.com/$number",
+      headers: {"Content-Type": "application/json"},
+    );
+
+    return NumberTriviaModel.fromJson(json.decode(response.body));
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() {
+    throw UnimplementedError();
+  }
 }
