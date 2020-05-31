01146035c02751d13ed0e8bf94689f0e29794c57
Williano <paawilly17@gmail.com>
2020-05-31 14:19:00 -0500

    Add implementation for bloc

--- lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
@@ -1,5 +1,10 @@
 import 'dart:async';
 
+import 'package:flutter_clean_architecture/core/error/failures_error.dart';
+import 'package:flutter_clean_architecture/core/utilities/presentation/input_converter.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/domain/entities/number_trivia_entity.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:meta/meta.dart';
 
 import 'package:bloc/bloc.dart';
@@ -8,7 +13,22 @@ import 'package:equatable/equatable.dart';
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    "Invalid Input = The number must be a positive integer or zero.";
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc(
+      {@required this.getConcreteNumberTrivia,
+      @required this.getRandomNumberTrivia,
+      @required this.inputConverter})
+      : assert(getConcreteNumberTrivia != null),
+        assert(getRandomNumberTrivia != null),
+        assert(inputConverter != null);
+
   @override
   NumberTriviaState get initialState => Empty();
 
@@ -16,6 +36,13 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   Stream<NumberTriviaState> mapEventToState(
     NumberTriviaEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+
+      yield* inputEither.fold((Failure failure) async* {
+        yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+      }, (integer) => null);
+    }
   }
 }
