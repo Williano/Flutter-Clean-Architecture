e07820468b570d639c2e8381fc0bf7d05fad2384
Williano <paawilly17@gmail.com>
2020-05-30 21:46:38 -0500

    Add bloc state

--- lib/features/number_trivia/presentation/bloc/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_state.dart
@@ -4,7 +4,30 @@ abstract class NumberTriviaState extends Equatable {
   const NumberTriviaState();
 }
 
-class NumberTriviaInitial extends NumberTriviaState {
+class Empty extends NumberTriviaState {
   @override
   List<Object> get props => [];
+}
+
+class Loading extends NumberTriviaState {
+  @override
+  List<Object> get props => [];
+}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia});
+
+  @override
+  List<Object> get props => [trivia];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message});
+
+  @override
+  List<Object> get props => [message];
 }
