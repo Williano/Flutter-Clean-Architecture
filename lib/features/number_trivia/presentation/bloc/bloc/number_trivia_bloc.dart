bc580baa882f4b48daf823e70a57d65e21120379
Williano <paawilly17@gmail.com>
2020-05-30 21:37:49 -0500

    Add bloc setup and event implementation

---
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  @override
+  NumberTriviaState get initialState => NumberTriviaInitial();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
